f865f7ded2db0b1b1aaf4d0b4e9a445c77c20528
Uriel <uriel_2012_666@hotmail.com>
2022-08-01 19:30:25 -0700

    creamos nuestra primera vista index OJO aun se neceasita mapear la URL para acceder a ella

--- miPagina/polls/views.py
+++ miPagina/polls/views.py
@@ -1,3 +1,11 @@
+import imp
 from django.shortcuts import render
 
 # Create your views here.
+
+#creamos nuestra primera vista "index html"
+
+from django.http import HttpResponse
+
+def index(request):
+    return HttpResponse("Hola Mundo , desde POLLS django")
