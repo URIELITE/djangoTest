d60727ed2b0059444f2028f4d5208686702f96fd
Uriel_Rodriguez <al16760261@ite.edu.mx>
2022-09-15 13:35:01 -0700

    creamos una vista. mas no el URL

--- polls/views.py
+++ polls/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+# cremos una nueva vista. esta vista es algun contenido , ojo no es la URL. AUN SE FALTA MAPEAR LA URL para acceder.
+
+from django.http import HttpResponse
+
+def index(request):
+    return HttpResponse("Hola mundo,  llamado desde polls index")
